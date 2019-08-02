928ca5a821bf94c494af6af20a54c5d1a72c8c2c
guilhermealbm <gui.miranda.13@gmail.com>
2019-08-02 12:16:35 -0300

    creating btnClick method

--- app/src/main/java/com/example/tictactoekotlin/MainActivity.kt
+++ app/src/main/java/com/example/tictactoekotlin/MainActivity.kt
@@ -2,11 +2,35 @@ package com.example.tictactoekotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun btnClick(view:View){
+        val btnSelected = view as Button
+        var cellID = 0;
+
+        when(btnSelected.id){
+            R.id.btn -> cellID = 1
+            R.id.btn2 -> cellID = 2
+            R.id.btn3 -> cellID = 3
+            R.id.btn4 -> cellID = 4
+            R.id.btn5 -> cellID = 5
+            R.id.btn6 -> cellID = 6
+            R.id.btn7 -> cellID = 7
+            R.id.btn8 -> cellID = 8
+            R.id.btn9 -> cellID = 9
+
+        }
+
+        Toast.makeText(this, "Cell Id: " + cellID, Toast.LENGTH_LONG).show()
+
     }
 }
